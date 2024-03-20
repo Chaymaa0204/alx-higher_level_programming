31fb32defe0e6660c695521a128482b1b3a01fb5
Chaymaa0204 <elbouzidichaymaa@gmail.com>
2024-03-20 14:00:11 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-from add_0 import add
+add_0 = __import__("add_0").add
+print("{} + {} = {}".format(a, b, add_0(a, b)))
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+# Importing the 0-add.py script
+__import__("0-add")
+
